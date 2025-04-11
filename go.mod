bb5d4e8d3103d5aeeb1680a53bedfb8a0cbb0421
Nicolae Nicora <n.nicora@sap.com>
2025-04-11 21:26:59 +0200

    Add utils package

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/openkcm/api-sdk
+
+go 1.24.2
---
+++ pkg/utils/utils.go
@@ -0,0 +1,9 @@
+package utils
+
+import (
+	"strings"
+)
+
+func Contains(s string, str string) bool {
+	return strings.Contains(s, str)
+}
